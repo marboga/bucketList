9a487fd595858f279a9e96693ee7f33437ec894e
Michael Arbogast <marboga@gmail.com>
2016-05-09 09:59:08 -0700

    added a BucketList ViewController, gave it displayability

---
+++ BucketList/BucketListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BucketList
+//
+//  Created by Michael Arbogast on 5/9/16.
+//  Copyright © 2016 Michael Arbogast. All rights reserved.
+//
+
+import UIKit
+
+class BucketListViewController: UITableViewController {
+    var missions = ["Sky diving", "Live in Hawaii"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // dequeue the cell from our storyboard
+        let cell = tableView.dequeueReusableCellWithIdentifier("MissionCell")!
+        // if the cell has a text label, set it to the model that is corresponding to the row in array
+        cell.textLabel?.text = missions[indexPath.row]
+        // return cell so that Table View knows what to draw in each row
+        return cell
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return missions.count
+    }
+}
