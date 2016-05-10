608378196c2f8531222658ded45ab3db2656b927
Michael Arbogast <marboga@gmail.com>
2016-05-10 15:12:14 -0700

    adding core data functionality.

---
+++ Mission+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Mission+CoreDataProperties.swift
+//  BucketList
+//
+//  Created by Michael Arbogast on 5/10/16.
+//  Copyright © 2016 Michael Arbogast. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Mission {
+
+    @NSManaged var details: String?
+
+}
---
+++ Mission.swift
@@ -0,0 +1,17 @@
+//
+//  Mission.swift
+//  BucketList
+//
+//  Created by Michael Arbogast on 5/10/16.
+//  Copyright © 2016 Michael Arbogast. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Mission: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
